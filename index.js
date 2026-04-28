3293aff13087ece95742693508147d9d9ae30f10
DevanshRathod12 <rathoddevansh53@gmail.com>
2026-04-28 23:42:32 +0530

    commit

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import service from './src/Services/service';
+
 import TrackPlayer from 'react-native-track-player';
 AppRegistry.registerComponent(appName, () => App);
-// TrackPlayer.registerPlaybackService(() => require('./src/Services/service'));
-TrackPlayer.registerPlaybackService(() => service);
+TrackPlayer.registerPlaybackService(() => require('./service'));
